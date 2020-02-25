f3343726b42a8ce2a0976efb007dec41c58f1fdd
claytonweaver <cweaver@cbctrenton.com>
2020-02-25 09:22:22 -0500

    lab

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab_18_Coffee_Shop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab_18_Coffee_Shop.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View(new User());
+        }
+
+        public IActionResult DisplayUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(user);
+            }
+            else
+            {
+                return View("Register",user);
+            }
+        }
+
+
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab_18_Coffee_Shop.Models
+{
+    public class User
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "Must be between 3 and 30 characters.")]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression(@"^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$")]
+        public string Email { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z]\w{3,14}$")]
+        public string Password { get; set; }
+    }
+}
